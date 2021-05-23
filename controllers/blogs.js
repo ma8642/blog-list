a28dfc97f081c11c68e7d726cc446c3a215753ee
Marley Alford <marleycalford@gmail.com>
2021-05-23 00:59:50 -0400

    Exercise 4.20

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,14 +3,6 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/blogList");
 const User = require("../models/user");
 
-const getTokenFrom = (request) => {
-  const authorization = request.get("authorization");
-  if (authorization && authorization.toLowerCase().startsWith("bearer")) {
-    return authorization.substring(7);
-  }
-  return null;
-};
-
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
@@ -18,11 +10,8 @@ blogsRouter.get("/", async (request, response) => {
 
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
-  const token = getTokenFrom(request);
-  console.log(token);
-  const decodedToken = jwt.verify(token, process.env.SECRET);
-  console.log(decodedToken);
-  if (!token || !decodedToken) {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  if (!request.token || !decodedToken) {
     return response.status(401).json({ error: "token missing or invalid" });
   }
   const user = await User.findById(decodedToken.id);
